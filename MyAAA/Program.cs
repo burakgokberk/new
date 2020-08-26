7cc2c3fc0fcc076ca77e1526325edd5922f1bcba
burakgokberk <burakgokberk88@gmail.com>
2020-08-26 17:29:51 +0300

    1

---
+++ MyAAA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyAAA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
